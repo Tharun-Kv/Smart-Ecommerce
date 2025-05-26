a4dd777de3641812181d8e4f6f12dad95d533a32
Tharun K V <your-email@example.com>
2025-05-26 17:18:45 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/signup";
 import Terms from "./pages/terms";
-import Welcome from"./pages/welcome";
+import Welcome from "./pages/welcome";
 import Cart from "./pages/cart";
 import CategoryPage from "./pages/CategoryPage";
-import Payment from"./pages/payment";
+import Payment from "./pages/payment";
 import OrderConfirmed from "./pages/OrderConfirmed";
-import Error404 from"./pages/Error404";
+import Error404 from "./pages/Error404";
+
 const App = () => {
   return (
     <Router>
@@ -16,16 +17,14 @@ const App = () => {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/welcome" element={<Welcome />}/>
         <Route path="/terms" element={<Terms />} />
+        <Route path="/welcome" element={<Welcome />} />
         <Route path="/cart" element={<Cart />} />
-<Route path="/category/:categoryName" element={<CategoryPage />} />
-               <Route path="/payment" element={<Payment />} />
-  <Route path="/order-confirmed" element={<OrderConfirmed />} /> {/* ✅ This route must be added */}
-  <Route path="/error-404" element={<Error404 />} />
-
-
-
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/category/:categoryName" element={<CategoryPage />} />
+        <Route path="/order-confirmed" element={<OrderConfirmed />} />
+        <Route path="/error-404" element={<Error404 />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </Router>
   );
