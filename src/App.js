49a6fcee9d8ff94ecf4184420f22b281530f4f0d
Tharun K V <your-email@example.com>
2025-06-13 16:09:20 +0530

    the updated version

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"; // 👈 Added Navigate
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom"; // 👈 Added Navigate
 import Login from "./pages/Login";
 import Signup from "./pages/signup";
 import Terms from "./pages/terms";
